08763ccec561412825c4c06fe5466a12b9c8a70c
Tim Handy <tjhandy@gmail.com>
2016-09-09 19:18:34 +0100

    Progress... added some functions

--- main.js
+++ main.js
@@ -5,13 +5,11 @@
 // User Story: My game will reset as soon as it's over so I can play again.
 
 
-
-
-
 //'use strict';  // Recommended as best practice, but disabled as issues with jshint stating "$ is not defined"
 
-
-// create a gameboard to store player positions
+var playerSymbol = "";
+var computerSymbol = "";
+var winner = false;
 
 var board = [
     null, null, null,
@@ -20,19 +18,10 @@ var board = [
 ];
 
 var availableCells = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-
 // board positions match the array positions:
 // 0 1 2
 // 3 4 5
 // 6 7 8
-
-
-// define player and computer symbols, x and x
-
-var playerSymbol = "";
-var computerSymbol = "";
-
-// array of winning plays
 
 var winningPlays = [
     [0,1,2],
@@ -44,81 +33,92 @@ var winningPlays = [
     [0,4,8],
     [2,4,6]
 ];
-// Ignore this... not sure if I will use zero-based or standard numbering
-//     [1,2,3],
-//     [4,5,6],
-//     [7,8,9],
-//     [1,4,7],
-//     [2,5,8],
-//     [3,6,9],
-//     [1,5,9],
-//     [3,5,7]
-// ];
 
-function play(board, playerSymbol, computerSymbol) {
+function askPlayerToChooseXorO() {
+    var playerInput = prompt("Would you like to be x or o?", "X or O").toUpperCase();
+    while (playerInput !== 'X' && playerInput !== 'O') {
+        playerInput = prompt("Invalid answer: Would you like to be x or o?", "X or O").toUpperCase();
+    }
+    return playerInput;
+}
 
-} // end play
+function setComputerSymbol(playerSymbol) {
+    if (playerSymbol === 'X') {
+        return 'O';
+    } else if (playerSymbol === 'O') {
+        return 'X';
+    }
+}
+
+function randomAvailableCell(availableCells) {
+    return availableCells[Math.floor(Math.random() * availableCells.length)];
+}
+
+function computerTurn(availableCells, board) {
+    // choose a random available cell
+    var cellChoice = randomAvailableCell(availableCells);
+    // update board with cellChoice
+    board[cellChoice] = computerSymbol;
+    // update availableCells array
+    availableCells.splice( availableCells.indexOf(cellChoice), 1);
+}
+
+function playerTurn() {
+    // do some stuff, click handlers and shit
+    //  if it is the users turn, wait for user to click on a square to choose, and the square is updated with the player piece
+}
+
+function checkWinner(board, computerSymbol, playerSymbol) {  // QUESTION: if the args here are globals, do we still need to pass them in, or can we just leave them out?
+    for (var i = 0; i < winningPlays.length; i++) {
+        if ( board[ winningPlays[i][0] ] === computerSymbol &&
+             board[ winningPlays[i][1] ] === computerSymbol &&
+             board[ winningPlays[i][2] ] === computerSymbol ) {
+                 winner = true;
+                 return 'Computer Wins... you moron!';
+                 //resetGame somehow
+        }
+        if
+        ( board[ winningPlays[i][0] ] === playerSymbol &&
+             board[ winningPlays[i][1] ] === playerSymbol &&
+             board[ winningPlays[i][2] ] === playerSymbol ) {
+                 winner = true;        // QUESTION: Is there a better way to do the winner and break out of the loop?
+                 return 'Player wins!';
+                 //resetGame somehow
+        }
+    }
+}
+
 
 $('document').ready(function(){
 
-    // ask whether player should be x or 0
+    // Wait for player to click Launch Game button
     $('#launch').click(function() {
 
-        var playerInput = prompt("Would you like to be x or o?", "X or O").toUpperCase();
-        while (playerInput !== 'X' && playerInput !== 'O') {
-            playerInput = prompt("Invalid answer: Would you like to be x or o?", "X or O").toUpperCase();
-        }
+        playerSymbol = askPlayerToChooseXorO();
+        console.log('Player chooses: ' + playerSymbol);
+        computerSymbol = setComputerSymbol(playerSymbol);
+        console.log('Computer Symbol: ' + computerSymbol);
 
-        playerSymbol = playerInput;
-        console.log('playerSymbol: ' + playerSymbol);
-        if (playerSymbol === 'X') {
-            computerSymbol = 'O';
-        } else if (playerSymbol === 'O') {
-            computerSymbol = 'X';
-        }
-        console.log('computerSymbol: ' + computerSymbol);
-
-        // Game starts
-        while (  availableCells.length > 0 ) { // while no winner and board not full  !winner &&
+        while (!winner) { // while no winner and board not full
             // computer goes first, chooses an available square (at random... later with AI?) and square is updated with piece
-            var randomAvailableCell = availableCells[Math.floor(Math.random() * availableCells.length)];
-            availableCells.splice( availableCells.indexOf(randomAvailableCell), 1);
-            board[randomAvailableCell] = computerSymbol;
-            //console.log('availableCells after splice: ' + availableCells);
-            //console.log('board after adding a position: ' + board);
-            // check for winner
-            for (var i = 0; i < winningPlays.length; i++) {
-                if ( board[ winningPlays[i][0] ] === computerSymbol &&
-                     board[ winningPlays[i][1] ] === computerSymbol &&
-                     board[ winningPlays[i][2] ] === computerSymbol ) {
-                         console.log('computer won!');  // seems to work!
-                         break;  // not sure how to break out and go back to the if and the while loops? Will this do it?
-                }
+            console.log('board before: ' + board);
+            console.log('availableCells before splice: ' + availableCells);
+            computerTurn(availableCells, board);
+            //playerTurn();
+            console.log( checkWinner(board, computerSymbol, playerSymbol) );
+            console.log('board after: ' + board);
+            console.log('availableCells after splice: ' + availableCells);
 
+            if (availableCells.length === 0) {  // extract to an isTie function?
+                console.log("It's a tie, tough luck");
+                // resetGame somehow
             }
-
-
-
         }
-
-
-        //  if it is the users turn, wait for user to click on a square to choose, and the square is updated with the player piece
-        //  then back and forth
-        // }
-
-        //play(board, playerSymbol, computerSymbol);
-
     });
-
 });  // end document ready
 
 
-// while (board contains at least one null or no winner) {
-//     //     computer goes first, chooses an available square (at random... later with AI?) and square is updated with piece
-//     //     check for winner
-//     //     if it is the users turn, wait for user to click on a square to choose, and the square is updated with the player piece
-//     //     then back and forth
-// }
+
 //
 // if (there is a winner) {
 //     declare the winner
